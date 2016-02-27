dd44f9d1c7192a0dfb846e8206e17f8e25b904ce
long <longv2j@gmail.com>
2016-02-27 16:56:03 +0800

    Initial Commit

---
+++ BlockInspectorTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockInspectorTest
+//
+//  Created by didi on 16/2/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockInspectorTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockInspectorTest
+//
+//  Created by didi on 16/2/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
